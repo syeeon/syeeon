e6b652ef7afc51e133b5bfa5be38bad39c0c1b89
syeeon <117874704+syeeon@users.noreply.github.com>
2022-11-24 09:08:31 +0900

    Delete php directory

--- php/sub/inc/dbcon.php
+++
@@ -1,4 +0,0 @@
-<?php
-$dbcon = mysqli_connect("localhost", "root", "", "aesop") or die("접속에 실패했습니다.");
-mysqli_set_charset($dbcon, "utf8");
-?>
--- php/sub/inc/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$login_idx = isset($_SESSION["login_idx"])? $_SESSION["login_idx"] : "";
-$login_name = isset($_SESSION["login_name"])? $_SESSION["login_name"] : "";
-$login_id = isset($_SESSION["login_id"])? $_SESSION["login_id"] : "";
-$login_pwd = isset($_SESSION["login_pwd"])? $_SESSION["login_pwd"] : "";
-
-?>
