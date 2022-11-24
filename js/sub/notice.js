8d2d5d1e729c553ab34ee34496e782e11e29a8e1
syeeon <117874704+syeeon@users.noreply.github.com>
2022-11-24 09:08:46 +0900

    Delete js directory

--- js/sub/notice.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".qst_box").click(function(){
-        $(".ans_box").not($(this).find(".ans_box")).slideUp();
-        $(this).find(".ans_box").slideToggle()
-    })
-
-})
